ccc4bfcc6993e59c3ecfe5bde8928d4098d30456
Heshan <heshan.imedha@gmail.com>
2021-05-20 17:59:09 +0530

    second commit

---
+++ recipe_app/lib/models/recipe.api.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+//import 'package:food_recipe/models/recipe.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:recipe_app/models/recipe.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "27e589a4c8mshf294c3b728d2075p1fb6b1jsn0eb53ff4d437",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
+
+
+
+
